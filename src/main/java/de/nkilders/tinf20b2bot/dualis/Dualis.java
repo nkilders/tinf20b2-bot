ff501fc0382d573d3f14a5876dc08e4001926132
nkilders <noah.kilders@gmail.com>
2021-05-12 10:54:59 +0200

    Add Dualis

---
+++ src/main/java/de/nkilders/tinf20b2bot/dualis/Dualis.java
@@ -0,0 +1,134 @@
+package de.nkilders.tinf20b2bot.dualis;
+
+import de.nkilders.tinf20b2bot.Bot;
+import de.nkilders.tinf20b2bot.Config;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Dualis extends ListenerAdapter {
+    private final File file = new File(Bot.BOT_FOLDER, "dualis.json");
+
+    @Override
+    public void onReady(@NotNull ReadyEvent event) {
+        if (!file.exists()) {
+            saveFile(loadDualis());
+        }
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                JSONArray dualis = loadDualis();
+                JSONArray local = loadFile();
+
+                if (dualis == null || local == null) return;
+                if (dualis.toString().equals(local.toString())) return;
+
+                for (int semester = 0; semester < dualis.length(); semester++) {
+                    if (local.length() <= semester) continue;
+
+                    JSONObject dSemester = dualis.getJSONObject(semester);
+                    JSONArray dModules = dSemester.getJSONArray("modules");
+
+                    JSONObject lSemester = local.getJSONObject(semester);
+                    JSONArray lModules = lSemester.getJSONArray("modules");
+
+                    for (int module = 0; module < dModules.length(); module++) {
+                        if (lModules.length() <= module) continue;
+
+                        JSONObject dModule = dModules.getJSONObject(module);
+                        JSONArray dExams = dModule.getJSONArray("exams");
+
+                        JSONObject lModule = lModules.getJSONObject(module);
+                        JSONArray lExams = lModule.getJSONArray("exams");
+
+                        for (int exam = 0; exam < dExams.length(); exam++) {
+                            if (lExams.length() <= exam) continue;
+
+                            JSONObject dExam = dExams.getJSONObject(exam);
+                            JSONObject lExam = lExams.getJSONObject(exam);
+
+                            if (lExam.getString("grade").equals("-") && !dExam.getString("grade").equals("-")) {
+                                sendMessage(
+                                        dSemester.getString("semester"),
+                                        dModule.getString("name"),
+                                        dExam.getString("exam")
+                                );
+                            }
+                        }
+                    }
+                }
+
+                saveFile(dualis);
+            }
+        }, 0, 1000 * 60 * 15);
+    }
+
+    private JSONArray loadDualis() {
+        try {
+            String[] cmd = {"/bin/bash", "resource/NOTEN.sh", "-u", Config.DUAlIS_USERNAME, "-p", Config.DUALIS_PASSWORD};
+            var p = Runtime.getRuntime().exec(cmd);
+            p.waitFor();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
+            StringBuilder sb = new StringBuilder();
+
+            while ((line = reader.readLine()) != null) {
+                sb.append(sb.length() == 0 ? "" : "\n").append(line);
+            }
+
+            return new JSONArray(sb.toString());
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private void saveFile(JSONArray json) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(file))) {
+            writer.print(json.toString());
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private JSONArray loadFile() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            StringBuilder sb = new StringBuilder();
+
+            while ((line = reader.readLine()) != null) {
+                sb.append(sb.length() == 0 ? "" : "\n").append(line);
+            }
+
+            return new JSONArray(sb.toString());
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private void sendMessage(String semester, String module, String exam) {
+        EmbedBuilder eb = new EmbedBuilder()
+                .setAuthor("Dualis")
+                .setTitle(semester + " - " + module)
+                .setDescription(exam)
+                .setColor(15158332);
+
+        TextChannel tc = Bot.jda.getTextChannelById(Config.DUALIS_CHANNEL_ID);
+        if (tc != null) {
+            tc.sendMessage(eb.build()).queue();
+        }
+    }
+}
