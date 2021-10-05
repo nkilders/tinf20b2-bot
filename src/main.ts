ef996dcb89a9db699ea954e2a9a3340bd0f5fabd
Noah Kilders <noah.kilders@sap.com>
2021-10-05 21:49:44 +0200

    Dualis

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
-console.log('Hallo Mama');
+import * as config from "./util/config";
+
+config.init();
+
+import { Client, Intents } from "discord.js";
+import * as dualis from "./dualis/dualis";
+
+const botConfig = config.load().bot;
+
+const bot = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+
+bot.once('ready', () => {
+    console.log('[Bot] Ready');
+    
+    dualis.start(bot);
+});
+
+bot.login(botConfig.token);
---
+++ src/util/config.ts
@@ -0,0 +1,61 @@
+import * as fs from "fs";
+
+export const DATA_DIR = './bot-data/';
+const CONFIG_FILE = DATA_DIR + 'config.json';
+
+export interface IConfig {
+    bot: IBotConfig;
+    rapla: IRaplaConfig;
+    dualis: IDualisConfig;
+}
+
+export interface IBotConfig {
+    token: string;
+}
+
+export interface IRaplaConfig {
+    url: string;
+    channelId: string;
+}
+
+export interface IDualisConfig {
+    username: string;
+    password: string;
+    channelId: string;
+}
+
+export function init() {
+    if(!fs.existsSync(DATA_DIR)) {
+        fs.mkdirSync(DATA_DIR);
+        fs.writeFileSync(
+            CONFIG_FILE,
+            JSON.stringify(blank(), null, 4)
+        );
+
+        console.log('Created config.json');
+        console.log('Go, enter your data ;)');
+
+        process.exit();
+    }
+}
+
+export function load(): IConfig {
+    return JSON.parse(fs.readFileSync(CONFIG_FILE).toString());
+}
+
+function blank(): IConfig {
+    return {
+        bot: {
+            token: ''
+        },
+        dualis: {
+            username: 'nachname.vorname@dh-karlsruhe.de',
+            password: '',
+            channelId: ''
+        },
+        rapla: {
+            url: 'https://rapla.dhbw-karlsruhe.de/rapla?page=ical&user=USER&file=FILE',
+            channelId: ''
+        }
+    };
+}
