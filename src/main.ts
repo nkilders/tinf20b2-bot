41db0c57dab4ae0e4bba773f94855f3bb90a05cf
nkilders <mail@nkilders.de>
2021-10-17 11:49:56 +0200

    AutoVC

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import * as config from "./util/config";
 config.init();
 
 import { Client, Intents } from "discord.js";
+import * as slashCommands from './util/slash-commands';
+import * as autovc from './autovc/listener';
 import * as dualis from "./dualis/dualis";
 
 const botConfig = config.load().bot;
@@ -10,13 +12,17 @@ const botConfig = config.load().bot;
 const bot = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_VOICE_STATES,
     ]
 });
 
 bot.once('ready', () => {
     console.log('[Bot] Ready');
     
+    slashCommands.registerListeners(bot);
+
+    autovc.registerListeners(bot);
     dualis.start(bot);
 });
 
