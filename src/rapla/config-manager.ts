6ed5ee9920c2f778d12f097dcb0f1d78b5ee31dc
nkilders <noah.kilders@gmail.com>
2022-05-28 20:08:04 +0200

    Add Rapla Command Handler + Config Manager

--- src/rapla/config-manager.ts
+++ src/rapla/config-manager.ts
@@ -2,58 +2,53 @@ import * as fs from 'fs';
 import { Guild } from "discord.js";
 import { DATA_DIR } from "../util/config";
 
+export const RAPLA_DIR = DATA_DIR + 'rapla/';
+
 const CONFIG_FILE = DATA_DIR + 'rapla.json';
 
 
-interface IConfig {
-    [guildId:string]: INotifierEntry[];
+interface IRaplaConfig {
+    [calendar:string]: IChannelData[];
 }
 
-interface INotifierEntry {
+export interface IChannelData {
+    guildId: string;
     channelId: string;
-    raplaUser: string;
-    raplaFile: string;
 }
 
 
-/**
- * Fügt einen neuen AutoVC-Datensatz der Config hinzu
- */
-export function onRegister(guildId: string, channelId: string, raplaUser: string, raplaFile: string) {
+export function onRegister(calendar: string, guildId: string, channelId: string) {
     const config = loadConfig();
 
-    if(!config[guildId]) {
-        config[guildId] = [];
+    if(!config[calendar]) {
+        config[calendar] = [];
     }
 
-    config[guildId].push({
+    // Notifier in Config einfügen
+    config[calendar].push({
+        guildId:   guildId,
         channelId: channelId,
-        raplaUser: raplaUser,
-        raplaFile: raplaFile,
     });
 
     saveConfig(config);
 }
 
-/**
- * Entfernt einen bestimmten AutoVC-Datensatz aus der Config
- */
-export function onUnregister(guildId: string, channelId: string, raplaUser: string, raplaFile: string) {
+export function onUnregister(calendar: string, guildId: string, channelId: string) {
     const config = loadConfig();
 
-    // Abbruch, wenn keine Notifier für den Server registriert sind
-    if(!config[guildId]) return;
+    // Abbruch, wenn keine Notifier für den Kalender registriert sind
+    if(!config[calendar]) return;
 
     // Notifier aus Config löschen
-    config[guildId] = config[guildId].filter(e => !(
-        e.channelId === channelId &&
-        e.raplaUser === raplaUser &&
-        e.raplaFile === raplaFile
+    config[calendar] = config[calendar].filter(e => !(
+        e.guildId   === guildId   &&
+        e.channelId === channelId
     ));
 
     // Server aus Config löschen, wenn keine Notifier mehr existieren
-    if(config[guildId].length === 0) {
-        delete config[guildId];
+    if(config[calendar].length === 0) {
+        delete config[calendar];
+        fs.rmSync(filePathFromCalendarName(calendar));
     }
 
     saveConfig(config);
@@ -65,23 +60,50 @@ export function onUnregister(guildId: string, channelId: string, raplaUser: stri
 export function onGuildDelete(guildId: string) {
     const config = loadConfig();
 
-    delete config[guildId];
+    for(const [calendar, channels] of Object.entries(config)) {
+        // Kanäle entfernen, die zu dem Server gehören
+        config[calendar] = channels.filter(c => !(
+            c.guildId === guildId
+        ));
+
+        // Kalender entfernen, falls es keine Kanäle mehr gibt
+        if(config[calendar].length === 0) {
+            delete config[calendar];
+            fs.rmSync(filePathFromCalendarName(calendar));
+        }
+    }
 
     saveConfig(config);
 }
 
 /**
- * Lädt alle AutoVC-Datensätze eines Servers aus der Config
+ * Lädt alle Rapla-Notifier eines Servers aus der Config
  * und gibt sie zurück
  */
-export function getGuildData(guild: Guild): INotifierEntry[] {
-    return loadConfig()[guild.id] || [];
+export function getGuildData(guild: Guild): IRaplaConfig | null {
+    const config = loadConfig();
+
+    for(const [calendar, channels] of Object.entries(config)) {
+        // Kanäle entfernen, die nicht zum Server gehören
+        config[calendar] = channels.filter(c => (
+            c.guildId === guild.id
+        ));
+
+        // Kalender entfernen, falls es keine Kanäle mehr gibt
+        if(config[calendar].length === 0) {
+            delete config[calendar];
+        }
+    }
+
+    if(Object.entries(config).length === 0) return null;
+
+    return config;
 }
 
 /**
  * Lädt die gesamte Config und gibt sie zurück
  */
-export function loadConfig(): IConfig {
+export function loadConfig(): IRaplaConfig {
     if(!fs.existsSync(CONFIG_FILE)) {
         return {};
     }
@@ -92,6 +114,10 @@ export function loadConfig(): IConfig {
 /**
  * Überschreibt die Config mit den übergebenen Daten
  */
-export function saveConfig(config: IConfig) {
+export function saveConfig(config: IRaplaConfig) {
     fs.writeFileSync(CONFIG_FILE, JSON.stringify(config, null, 2));
+}
+
+export function filePathFromCalendarName(name: string): string {
+    return RAPLA_DIR + name.replace('/', '-') + '.ics';
 }
--- src/rapla/listener.ts
+++ src/rapla/listener.ts
@@ -1,8 +1,13 @@
 import { Client } from "discord.js";
 import * as commandHandler from './command-handler';
+import * as configMngr from './config-manager';
 
 export function registerListeners(bot: Client) {
-    bot.on('interactionCreate', async interaction => {
+    bot.on('guildDelete', guild => {
+        configMngr.onGuildDelete(guild.id);
+    })
+    
+    .on('interactionCreate', async interaction => {
         if(!interaction.isCommand()) return;
         if(interaction.commandName !== 'rapla') return;
 
